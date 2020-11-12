d99defc3e1ad8e88341f36cea15416e63b2d6933
ishunkina-m <74357055+ishunkina-m@users.noreply.github.com>
2020-11-12 19:45:57 +0300

    math

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram1.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram1.java
@@ -4,5 +4,9 @@ public class MyFirstProgram1 {
 
     public static void main(String[] args) {
         System.out.println("Hello,world!");
+        System.out.println(2+6);
+        System.out.println(2-1);
+        System.out.println(1.0/2);
+        System.out.println("2"+2);
     }
 }
