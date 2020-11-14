98d0b19a5ae8f1b4dbbd1c07e600f9e0825d0bc9
ishunkina-m <74357055+ishunkina-m@users.noreply.github.com>
2020-11-14 15:07:09 +0300

    последние изменения

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/square.java
@@ -0,0 +1,11 @@
+package ru.stqa.pft.sandbox;
+
+public class square {
+    public double l;
+public square(double l){
+    this.l=l;
+}
+    public  double area() {
+        return this.l * this.l;
+    }
+}
